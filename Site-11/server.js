e0470859479699e9018e2b0c211d0f280e614e87
ATroubledSnake <71598450+dnumde123456@users.noreply.github.com>
2024-09-12 13:03:53 +0200

    Add files via upload

---
+++ Site-11/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'site-11.html'));
+});
+
+app.get('/css/:file', (req, res) => {
+  res.sendFile(path.join(__dirname, 'css', req.params.file));
+});
+
+app.get('/js/:file', (req, res) => {
+  res.sendFile(path.join(__dirname, 'js', req.params.file));
+});
+
+app.get('/images/:file', (req, res) => {
+  res.sendFile(path.join(__dirname, 'images', req.params.file));
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
